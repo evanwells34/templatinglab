5377eaebfac73bdde1f8d43135efb51b57ab5f55
evanwells34 <ewells0709@gmail.com>
2024-11-03 21:22:19 -0600

    11/4

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,127 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-interface Ingredient{
+interface Ingredient {
     String getName();
+
     double getQuantity();
 }
-class SolidIngredient implements Ingredient{
+
+class SolidIngredient implements Ingredient {
     private String name;
-    private double quantity;
-    public SolidIngredient(String name, double quantity){
-        this.name = name;
-        this.quantity = quantity;
+    private double qtyGrams;
+
+    public SolidIngredient(String n, double q) {
+        name = n;
+        qtyGrams = q;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public double getQuantity(){
-        return quantity;
+
+    public double getQuantity() {
+        return qtyGrams;
     }
 }
-class LiquidIngredient implements Ingredient{
+
+class LiquidIngredient implements Ingredient {
     private String name;
-    private double quantity;
-    public LiquidIngredient(String name, double quantity){
-        this.name = name;
-        this.quantity = quantity;
+    private double qtyMl;
+
+    public LiquidIngredient(String n, double q) {
+        name = n;
+        qtyMl = q;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public double getQuantity(){
-        return quantity;
+
+    public double getQuantity() {
+        return qtyMl;
     }
 }
-class Recipe<T extends Ingredient>{
+
+class Recipe<T extends Ingredient> {
     private String name;
     private String instructions;
     private ArrayList<T> ingredients;
-    public Recipe(String name, String instructions, ArrayList<T> ingredients, int size){
-        this.name = name;
-        this.instructions = instructions;
-        this.ingredients  = new ArrayList<T>(size);
+
+    public Recipe(String n, String inst) {
+        name = n;
+        instructions = inst;
+        ingredients = new ArrayList<T>();
     }
-    public void addIngredient(T ingredient){
-        this.ingredients.add(ingredient);
+
+    public void addIngredient(T t) {
+        ingredients.add(t);
     }
-    public void print(){
-        if (ingredients == null){
+
+    public void print() {
+        if (ingredients.isEmpty()) {
             System.out.println("No ingredients found");
-        }
-        else{
-            System.out.println("Name: " + name + "Instructions: " + instructions + "Ingredients: " + ingredients);
+        } else {
+            int i = 0;
+            for(T ingredient : ingredients) {
+                System.out.println("Name: " + ingredients.get(i).getName() + " Quantity: " + ingredients.get(i).getQuantity());
+                i++;
+            }
         }
     }
 
 }
+
 public class Main {
     public static void main(String[] args) {
-        // Need To Fix This
-        Recipe<Ingredient> = new Recipe<Ingredient>
-        Recipe r = new Recipe("soup", "Stir", T, 6);
+        Recipe<Ingredient> recipe;
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+        String instruction = "";
+        recipe = new Recipe<Ingredient>(name, instruction);
+
+        System.out.println("Welcome To the Recipe Site!");
+        System.out.println("1. Type 1 to add ingredient");
+        System.out.println("2. Type 2 to list ingredients");
+        System.out.println("3. Type 3 to exit");
+        try {
+        int userInput = sc.nextInt();
+            sc.nextLine();
+            while (userInput != 3) {
+                if (userInput == 1) {
+                    addIngredient(recipe, sc);
+                    System.out.println("1. Type 1 to add ingredient");
+                    System.out.println("2. Type 2 to list ingredients");
+                    userInput = sc.nextInt();
+                }
+                else if (userInput == 2) {
+                    recipe.print();
+                    System.out.println("1. Type 1 to add ingredient");
+                    System.out.println("2. Type 3 to exit");
+                    userInput = sc.nextInt();
+                }
+                else {
+                    System.out.println("Invalid Choice");
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid Input: " + e.getMessage());
+            System.exit(-1);
+        }
     }
+    public static void addIngredient(Recipe<Ingredient> recipe, Scanner sc) {
+        System.out.println("Type s for solid ingredient, type l for liquid ingredient");
+        char type = sc.next().toLowerCase().charAt(0);
+        System.out.println("Enter ingredient");
+        String name = sc.next();
+        System.out.println("Enter quantity");
+        double quantity = Double.parseDouble(sc.next());
+        Ingredient ingredient;
+        if (type == 's')
+            ingredient = new SolidIngredient(name, quantity);
+        else
+            ingredient = new LiquidIngredient(name, quantity);
+        recipe.addIngredient(ingredient);
+    }
+
 }
